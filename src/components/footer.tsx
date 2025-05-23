dddec13c01c9915ecc69802ace668efc3e070eb2
Natalie B <22222885+espeon@users.noreply.github.com>
2025-05-23 10:11:37 -0500

    add links to footer oops

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,10 @@ import { SiBluesky, SiGit, SiGithub } from "@icons-pack/react-simple-icons";
 
 export default function Footer() {
   return (
-    <footer className="bg-teal-950 py-8 text-start max-w-full border-t-2 border-border">
-      <div className="container mx-auto px-4 flex justify-between items-center">
+    <footer className="bg-teal-950 py-8 text-center md:text-start max-w-full border-t-2 border-border">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row gap-4 justify-between items-center">
         <p className="text-sm">
-          Teal Computing LLC.
+          Teal Computing LLC
           <br />
           A Delaware limited liability company.
           <br />
@@ -13,9 +13,20 @@ export default function Footer() {
           reserved.
         </p>
         <div className="flex gap-2">
-          <SiBluesky className="h-6 w-6 mx-2 hover:text-[#00b7ff] transition-colors" />
-          <SiGithub className="h-6 w-6 mx-2 hover:text-[#fff] transition-colors" />
-          <SiGit className="h-6 w-6 mx-2 hover:text-[#fff] transition-colors" />
+          <a
+            href="https://bsky.app/profile/teal.fm"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-[#00b7ff] transition-colors"
+          >
+            <SiBluesky className="h-6 w-6 mx-2 hover:text-[#00b7ff] transition-colors" />
+          </a>
+          <a href="https://github.com/teal-fm" target="_blank">
+            <SiGithub className="h-6 w-6 mx-2 hover:text-[#fff] transition-colors" />
+          </a>
+          <a href="https://tangled.sh/teal.fm" target="_blank">
+            <SiGit className="h-6 w-6 mx-2 hover:text-[#fff] transition-colors" />
+          </a>
         </div>
       </div>
     </footer>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,13 +12,13 @@ function Home() {
       <div className="flex flex-col justify-center items-center flex-1 gap-3 relative">
         <div className="relative">
           <div className="absolute flex justify-center items-center -rotate-2 -z-10 w-full h-full">
-            <div className="bg-teal-800 absolute w-9/10 h-8/10 -z-10" />
+            <div className="bg-teal-800 absolute w-12/10 h-11/10 md:w-9/10 md:h-8/10 -z-10" />
           </div>
-          <h1 className="font-display text-6xl md:text-7xl font-thin w-max h-min">
+          <h1 className="font-display text-6xl md:text-7xl font-thin w-min md:w-max h-min">
             Teal Computing Co.
           </h1>
         </div>
-        <h3 className="text-xl md:text-2xl font-normal w-max h-min">
+        <h3 className="text-md md:text-2xl font-normal w-max h-min">
           interesting solutions for interesting problems
         </h3>
       </div>
